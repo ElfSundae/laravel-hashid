4439c1b9ed6f0fa0f5a6fd6ab53fdc5fdeb4e97e
Elf Sundae <elf.sundae@gmail.com>
2017-10-10 01:06:40 +0800

    Use urlsafe-base64 package

--- src/helpers.php
+++ src/helpers.php
@@ -41,37 +41,6 @@ if (! function_exists('hashid_decode')) {
     }
 }
 
-if (! function_exists('urlsafe_base64_encode')) {
-    /**
-     * Encodes the given data with base64, and returns an URL-safe string.
-     *
-     * @see https://github.com/ElfSundae/laravel-helper
-     *
-     * @param  string  $data
-     * @return string
-     */
-    function urlsafe_base64_encode($data)
-    {
-        return strtr(base64_encode($data), ['+' => '-', '/' => '_', '=' => '']);
-    }
-}
-
-if (! function_exists('urlsafe_base64_decode')) {
-    /**
-     * Decodes a base64 encoded data.
-     *
-     * @see https://github.com/ElfSundae/laravel-helper
-     *
-     * @param  string  $data
-     * @param  bool  $strict
-     * @return string
-     */
-    function urlsafe_base64_decode($data, $strict = false)
-    {
-        return base64_decode(strtr($data.str_repeat('=', (4 - strlen($data) % 4)), '-_', '+/'), $strict);
-    }
-}
-
 if (! function_exists('config_path')) {
     /**
      * Get the configuration path.
