a6e8b181a25389ca4b8739d862b5434e1910cdc5
Elf Sundae <elf.sundae@gmail.com>
2017-10-10 03:48:26 +0800

    testFacades

--- tests/HashidServiceProviderTest.php
+++ tests/HashidServiceProviderTest.php
@@ -5,6 +5,7 @@ namespace ElfSundae\Laravel\Hashid\Test;
 use Orchestra\Testbench\TestCase;
 use ElfSundae\Laravel\Hashid\HashidManager;
 use ElfSundae\Laravel\Hashid\HexConnection;
+use ElfSundae\Laravel\Hashid\Facades\Hashid;
 use ElfSundae\Laravel\Hashid\HashidServiceProvider;
 
 class HashidServiceProviderTest extends TestCase
@@ -13,6 +14,11 @@ class HashidServiceProviderTest extends TestCase
     {
         $this->assertSame($this->app['hashid'], $this->app[HashidManager::class]);
         $this->assertSame($this->app['hashid.connection.hex'], $this->app[HexConnection::class]);
+    }
+
+    public function testFacades()
+    {
+        $this->assertSame($this->app['hashid'], Hashid::getFacadeRoot());
     }
 
     protected function getPackageProviders($app)
