2e044e567a6562d403761d7ddf039535701cdba8
Elf Sundae <elf.sundae@gmail.com>
2022-05-02 03:11:08 +0800

    Add PHPDoc for Hashid facade

--- src/Facades/Hashid.php
+++ src/Facades/Hashid.php
@@ -5,7 +5,15 @@ namespace ElfSundae\Laravel\Hashid\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \ElfSundae\Laravel\Hashid\DriverInterface connection(?string $name = null)
+ * @method static \ElfSundae\Laravel\Hashid\DriverInterface driver(?string $name = null)
+ * @method static string getDefaultConnection()
+ * @method static \ElfSundae\Laravel\Hashid\HashidManager setDefaultConnection(string $name)
+ * @method static mixed encode(mixed $data)
+ * @method static mixed decode(mixed $data)
+ *
  * @see \ElfSundae\Laravel\Hashid\HashidManager
+ * @see \ElfSundae\Laravel\Hashid\DriverInterface
  */
 class Hashid extends Facade
 {
